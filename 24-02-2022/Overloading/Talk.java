ab124dcb7662ffa5fe1f9fc8fefe153e7e4d6ac7
lohitkumarvellore <96835018+lohitkumarvellore@users.noreply.github.com>
2022-03-05 14:07:26 +0530

    Add files via upload

---
+++ 24-02-2022/Overloading/Talk.java
@@ -0,0 +1,15 @@
+public class Talk{
+        public static void talk()
+        {
+            System.out.println("hello there!");
+        }
+        public static void  talk(String s){
+
+            System.out.println("hello " + s);
+
+        }
+        public static void main(String[] args)
+        {
+            talk("Matthew");
+        }
+}
